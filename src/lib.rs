a1d577e9bb721835e47da548612b3cdb89e38b5a
vhnat@eruptiq <vhnatyk@gmail.com>
2019-07-29 22:40:59 +0300

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -27,38 +27,38 @@ mod secp256k1instance {
 #[cfg(feature = "curvesecp256k1")]
 pub use self::secp256k1instance::*;
 
-#[cfg(feature = "curveristretto")]
-mod curveristrettoinstance {
-    pub use elliptic::curves::curve_ristretto::FE;
-    pub use elliptic::curves::curve_ristretto::GE;
-    pub use elliptic::curves::curve_ristretto::PK;
-    pub use elliptic::curves::curve_ristretto::SK;
-}
+// #[cfg(feature = "curveristretto")]
+// mod curveristrettoinstance {
+//     pub use elliptic::curves::curve_ristretto::FE;
+//     pub use elliptic::curves::curve_ristretto::GE;
+//     pub use elliptic::curves::curve_ristretto::PK;
+//     pub use elliptic::curves::curve_ristretto::SK;
+// }
 
-#[cfg(feature = "curveristretto")]
-pub use self::curveristrettoinstance::*;
+// #[cfg(feature = "curveristretto")]
+// pub use self::curveristrettoinstance::*;
 
-#[cfg(feature = "ed25519")]
-mod ed25519instance {
-    pub use elliptic::curves::ed25519::FE;
-    pub use elliptic::curves::ed25519::GE;
-    pub use elliptic::curves::ed25519::PK;
-    pub use elliptic::curves::ed25519::SK;
-}
+// #[cfg(feature = "ed25519")]
+// mod ed25519instance {
+//     pub use elliptic::curves::ed25519::FE;
+//     pub use elliptic::curves::ed25519::GE;
+//     pub use elliptic::curves::ed25519::PK;
+//     pub use elliptic::curves::ed25519::SK;
+// }
 
-#[cfg(feature = "ed25519")]
-pub use self::ed25519instance::*;
+// #[cfg(feature = "ed25519")]
+// pub use self::ed25519instance::*;
 
-#[cfg(feature = "curvejubjub")]
-mod jubjubinstance {
-    pub use elliptic::curves::curve_jubjub::FE;
-    pub use elliptic::curves::curve_jubjub::GE;
-    pub use elliptic::curves::curve_jubjub::PK;
-    pub use elliptic::curves::curve_jubjub::SK;
-}
+// #[cfg(feature = "curvejubjub")]
+// mod jubjubinstance {
+//     pub use elliptic::curves::curve_jubjub::FE;
+//     pub use elliptic::curves::curve_jubjub::GE;
+//     pub use elliptic::curves::curve_jubjub::PK;
+//     pub use elliptic::curves::curve_jubjub::SK;
+// }
 
-#[cfg(feature = "curvejubjub")]
-pub use self::jubjubinstance::*;
+// #[cfg(feature = "curvejubjub")]
+// pub use self::jubjubinstance::*;
 
 pub mod arithmetic;
 #[cfg(feature = "gmp")]
